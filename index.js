590b256b3691029b67a66ea5aa20ba144d4bd970
Stanionis <estanionis@gmail.com>
2022-07-10 10:53:24 +0300

    docker, mysql

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Hello! Welcome. </h1>`);
-});
+let db;
 
-app.listen(3000);
+async function go() {
+  db = await mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "example",
+    database: "test",
+  });
+  app.listen(3000);
+}
+
+go();
+
+app.get("/", async (req, res) => {
+  const [kates] = await db.execute("SELECT * FROM kates");
+  console.log(kates);
+  res.send(
+    `<ul>${kates.map((gyvunai) => `<li>${gyvunai.name}</li>`).join("")}</ul>`
+  );
+});
